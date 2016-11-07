7266ec0f6e3a0600a08ed74e6ace8de7876c742b
jdi4 <jzxp.a4t@gmail.com>
2016-11-07 23:03:48 +0100

    SignalR CORS configuration

--- Cypisek/App_Start/Startup.cs
+++ Cypisek/App_Start/Startup.cs
@@ -5,8 +5,10 @@ using System.Web;
 
 using Owin;
 using Microsoft.Owin;
+using Microsoft.Owin.Cors;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNet.SignalR;
 
 [assembly: OwinStartup(typeof(Cypisek.Startup))]
 
@@ -17,7 +19,26 @@ namespace Cypisek
         public void Configuration(IAppBuilder app)
         {
             // Any connection or hub wire up and configuration should go here
-            app.MapSignalR();
+            app.Map("/signalr", map =>
+            {
+                // Setup the CORS middleware to run before SignalR.
+                // By default this will allow all origins. You can 
+                // configure the set of origins and/or http verbs by
+                // providing a cors options with a different policy.
+                map.UseCors(CorsOptions.AllowAll);
+                var hubConfiguration = new HubConfiguration
+                {
+                    // You can enable JSONP by uncommenting line below.
+                    // JSONP requests are insecure but some older browsers (and some
+                    // versions of IE) require JSONP to work cross domain
+                    // EnableJSONP = true
+                };
+                // Run the SignalR pipeline. We're not using MapSignalR
+                // since this branch already runs under the "/signalr"
+                // path.
+                map.RunSignalR(hubConfiguration);
+            });
+            //app.MapSignalR();
         }
 
 
