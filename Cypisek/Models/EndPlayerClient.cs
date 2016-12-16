8aae38c624eb830b632f000210618768fecdd9f2
jdi4 <jzxp.a4t@gmail.com>
2016-12-16 19:16:03 +0100

    Added models

---
+++ Cypisek/Models/EndPlayerClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cypisek.Models
+{
+    public class EndPlayerClient
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        // FK
+        public int ClientGroupID { get; set; }
+        public int ClientScheduleID { get; set; }
+
+        // Nav.properties
+        public ClientGroup ClientGroup { get; set; }
+        public ClientSchedule ClientSchedule { get; set; }
+    }
+}
