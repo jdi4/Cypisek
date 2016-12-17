318af2dafcad49358250787dae07e1549bb01a90
jdi4 <jzxp.a4t@gmail.com>
2016-12-17 23:10:49 +0100

    Temp stub

---
+++ Cypisek/Controllers/SchedulesController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cypisek.Controllers
+{
+    public class SchedulesController : Controller
+    {
+        // GET: Schedules
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Schedules/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Schedules/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Schedules/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Schedules/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Schedules/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Schedules/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Schedules/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Cypisek/ViewModels/Schedules/ClientScheduleViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Cypisek.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cypisek.ViewModels.Schedules
+{
+    public class ClientScheduleViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+        public int PlaylistCount
+        {
+            get { return MediaPlaylist.Count; }
+        }
+
+        public int ClientsCount
+        {
+            get { return EndPlayerClients.Count; }
+        }
+
+        public virtual ICollection<ClientScheduleMediaFilesList> MediaPlaylist { get; set; }
+        public virtual ICollection<EndPlayerClient> EndPlayerClients { get; set; }
+    }
+}
