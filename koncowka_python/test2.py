e84509e0f4d75e753ce4d5a6a82128d396cc2bb1
slawomirpolak <slawomirpolak93@gmail.com>
2016-12-17 18:09:32 +0100

    Add Internetcheck()

--- koncowka_python/test2.py
+++ koncowka_python/test2.py
@@ -1,36 +1,88 @@
 from requests import Session
 from requests.auth import HTTPBasicAuth
 from signalr import Connection
+import urllib2
+import wx
 
 
+class Panel1(wx.Panel):
+   
+    def __init__(self, parent, id):
+        # create the panel
+        wx.Panel.__init__(self, parent, id)
+        
+
+    def Load(self,image_file,image_name):
+        try:
+            # pick an image file you have in the working 
+            # folder you can load .jpg  .png  .bmp  or 
+            # .gif files
+           
+            bmp1 = wx.Image(image_file,wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+            # image's upper left corner anchors at panel 
+            # coordinates (0, 0)
+            self.bitmap1 = wx.StaticBitmap(self, -1, bmp1, (0, 0))
+            # show some image details
+            str1 = "%s  %dx%d" % (image_file, bmp1.GetWidth(),bmp1.GetHeight()) 
+            '''parent.SetTitle(str1)'''
+        except IOError:
+            print "Image file %s not found" % imageFile
+            raise SystemExit
+ 
+       
+ 
+app = wx.App(False)
+frame1 = wx.Frame(None, -1, "An image on a panel", 
+size=(400, 400))
+frame1.Show(True)             
+# create the class instance
+panel1 = Panel1(frame1, -1)
+ 
+def internetCheck():
+    try :
+        url = "http://cypisek.azurewebsites.net"
+        urllib2.urlopen(url);
+        print"Connected"
+        return 1
+    except :
+        print "Not connect"
+        return 0
+    
+ 
 def main():
-    with Session() as session:
-        session.auth = HTTPBasicAuth("known", "user")
-        connection = Connection("http://localhost:5000/signalr", session)
-        chat = connection.register_hub('chat')
-
-        def print_received_message(data):
-            print('received: ', data)
-
-        def print_topic(topic, user):
-            print('topic: ', topic, user)
-
-        def print_error(error):
-            print('error: ', error)
-
-        chat.client.on('newMessageReceived', print_received_message)
-        chat.client.on('topicChanged', print_topic)
-
-        connection.error += print_error
-
-        with connection:
-            chat.server.invoke('send', 'Python is here')
-            chat.server.invoke('setTopic', 'Welcome python!')
-            chat.server.invoke('requestError')
-            chat.server.invoke('send', 'Bye-bye!')
-
-            connection.wait(1)
-
+    if (internetCheck()): 
+        with Session() as session:
+            
+            try:
+                connection = Connection("http://cypisek.azurewebsites.net/signalr", session)
+               
+            except IOError:
+                print "Working without Internet Connection"
+            chat = connection.register_hub('helloTestHub')
+            file_name=''
+            def setImage(file_name, url):
+                print('udalo sie')
+                print( 'ustawiono zdjecie:', file_name, url)
+                
+                file_name+='.jpg'
+                with open(file_name,'wb') as f:
+                    f.write(urllib2.urlopen(url).read())
+                    f.close()
+                    panel1.Load(file_name, "test")
+                # create a window/frame, no parent, -1 is default ID
+                # change the size of the frame to fit the backgound images
+    
+                
+                app.MainLoop()
+    
+            chat.client.on('setImage', setImage)  
+            chat.client.on('setImage', setImage)  
+            with connection:
+              connection.wait(10)  
+    
 
 if __name__ == "__main__":
     main()
+    
+    
+    
