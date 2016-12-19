1f5f875145b7efb64a8f091e92d39f206ff18e7e
jdi4 <jzxp.a4t@gmail.com>
2016-12-19 23:08:57 +0100

    Added main hub for clients ContentHub and added DB-model connection status

--- Cypisek.Data/CypisekSeedData.cs
+++ Cypisek.Data/CypisekSeedData.cs
@@ -40,24 +40,28 @@ namespace Cypisek.Data
             {
                 new EndPlayerClient()
                 {
+                    ID = 1,
                     Name = "Końcówka 1",
                     ClientGroupID = 2,
                     LastConnectionDate = DateTime.Now,
-                    IsSynchronized = false
+                    IsSynchronized = false,
+                    IsConnected = false
                 },
                 new EndPlayerClient()
                 {
                     Name = "Końcówka 2",
                     ClientGroupID = 1,
                     LastConnectionDate = DateTime.Now,
-                    IsSynchronized = true
+                    IsSynchronized = true,
+                    IsConnected = false
                 },
-                                new EndPlayerClient()
+                new EndPlayerClient()
                 {
                     Name = "Końcówka 3",
                     ClientGroupID = 2,
                     LastConnectionDate = DateTime.Now,
-                    IsSynchronized = false
+                    IsSynchronized = false,
+                    IsConnected = false
                 }
             };
         }
---
+++ Cypisek/Hubs/ContentHub.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNetCore.Cors;
+using Cypisek.Services;
+
+namespace SignalRTest
+{
+    [EnableCors("AllowSpecificOrigin")]
+    public class ContentHub : Hub
+    {
+        private readonly IEndPlayerClientService endPlayerClientService;
+
+        public ContentHub(IEndPlayerClientService epcS)
+        {
+            this.endPlayerClientService = epcS;
+        }
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void PoorAuthenticate(int clientid)
+        {
+            var client = endPlayerClientService.GetEndPlayerClient(clientid);
+
+            if (client != null)
+            {
+                Clients.Caller.confirm(true);
+            }
+        }
+    }
+}
