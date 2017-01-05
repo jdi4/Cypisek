ae006af9e25253b81380024174308cc7f6094247
jdi4 <jzxp.a4t@gmail.com>
2017-01-05 12:12:58 +0100

    DropDownLists for groups and schedules

--- Cypisek/ViewModels/Clients/ClientManagerViewModel.cs
+++ Cypisek/ViewModels/Clients/ClientManagerViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Cypisek.ViewModels.Clients
 {
@@ -10,5 +11,23 @@ namespace Cypisek.ViewModels.Clients
     {
         public ICollection<ClientGroupViewModel> ClientsGroups { get; set; }
         public ICollection<EndPlayerClientViewModel> ClientsWithoutGroup { get; set; }
+
+        public ICollection<SelectListItem> ClientsGroupsSL
+        {
+            get
+            {
+                //List<SelectListItem> slilist = new List<SelectListItem>();
+
+                //foreach (var group in this.ClientsGroups)
+                //{
+                //    slilist.Add(new SelectListItem() { Text = group.Name, Value = group.ID.ToString() });
+                //}
+                return this.ClientsGroups
+                    .Select( g => new SelectListItem { Text = g.Name, Value = g.ID.ToString() })
+                    .ToList();
+            }
+        }
+
+        public ICollection<SelectListItem> ClientsSchedulesSL { get; set; }
     }
 }
