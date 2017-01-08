f12ff99ff88db1f0244754f414974662bbef1a3f
jdi4 <jzxp.a4t@gmail.com>
2017-01-08 02:27:12 +0100

    Clients form model
    For setting groups and schedules

---
+++ Cypisek/ViewModels/Clients/ClientManagerFormModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cypisek.ViewModels.Clients
+{
+    public class ClientManagerFormModel
+    {
+        public int? ClientsGroupsSL { get; set; }
+        public int? ClientsSchedulesSL { get; set; }
+        public List<EndPlayerClientViewModel> ClientsList { get; set; }
+    }
+}
