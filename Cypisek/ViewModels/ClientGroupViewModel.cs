35fe59beca9f336bce5bd831659c6d04d827640d
jdi4 <jzxp.a4t@gmail.com>
2017-01-09 22:12:41 +0100

    ViewModels Names

--- Cypisek/ViewModels/ClientGroupViewModel.cs
+++ Cypisek/ViewModels/ClientGroupViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace Cypisek.ViewModels
     public class ClientGroupViewModel
     {
         public int ID { get; set; }
+
+        [DisplayName("Nazwa")]
         public string Name { get; set; }
 
         public virtual List<EndPlayerClientViewModel> EndPlayerClients { get; set; }
--- Cypisek/ViewModels/EndPlayerClientViewModel.cs
+++ Cypisek/ViewModels/EndPlayerClientViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Cypisek.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -9,9 +10,17 @@ namespace Cypisek.ViewModels
     public class EndPlayerClientViewModel
     {
         public int ID { get; set; }
+
+        [DisplayName("Nazwa")]
         public string Name { get; set; }
+
+        [DisplayName("Ostatnie połączenie")]
         public DateTime LastConnectionDate { get; set; }
+
+        [DisplayName("Jest podłączony")]
         public bool IsConnected { get; set; }
+
+        [DisplayName("Jest zsynchronizowany")]
         public bool IsSynchronized { get; set; }
 
         public int ClientGroupID { get; set; }
@@ -21,6 +30,8 @@ namespace Cypisek.ViewModels
         public ClientSchedule ClientSchedule { get; set; }
 
         // extra properties
+
+        [DisplayName("Harmonogram")]
         public string ScheduleName
         {
             get { return ClientSchedule.Name; }
